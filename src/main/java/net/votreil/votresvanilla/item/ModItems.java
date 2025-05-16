e82b3952de4da7156e435107b995cd6636fa74d1
Votre-IL <adamgaltier@icloud.com>
2025-05-16 18:14:13 +0200

    fixed emerald shards textures & model added copper horn fixed gradle properties

--- src/main/java/net/votreil/votresvanilla/item/ModItems.java
+++ src/main/java/net/votreil/votresvanilla/item/ModItems.java
@@ -5,22 +5,31 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import net.votreil.votresvanilla.VotresVanilla;
 
 public class ModItems {
 
-    public static final Item EMERALD_SHARD = registerItem("emerald_shard", new Item(new Item.Settings()));
+    public static final Item EMERALD_SHARD = registerItem("emerald_shard", new Item(new Item.Settings()
+            .registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(VotresVanilla.MOD_ID, "emerald_shard")))));
+    public static final Item COPPER_HORN = registerItem("copper_horn", new Item(new Item.Settings()
+            .registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(VotresVanilla.MOD_ID, "copper_horn")))));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(VotresVanilla.MOD_ID, name), item);
     }
 
     public static void registerModItems() {
-        VotresVanilla.LOGGER.info("Registering Mod Itemps for" + VotresVanilla.MOD_ID);
+        VotresVanilla.LOGGER.info("Registering Mod Items for" + VotresVanilla.MOD_ID);
 
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(entries -> {
+            entries.add(COPPER_HORN);
+        });
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(EMERALD_SHARD);
         });
+
     }
 }
