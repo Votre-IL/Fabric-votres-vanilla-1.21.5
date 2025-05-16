d61cac5dbc795473282666661332369f2d53acff
Adam GALTIER <adamgaltier@macbook-air-de-adam-4.home>
2025-05-16 15:16:19 +0200

    Initial commit

---
+++ src/main/java/net/votreil/votresvanilla/VotresVanilla.java
@@ -0,0 +1,17 @@
+package net.votreil.votresvanilla;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class VotresVanilla implements ModInitializer {
+	public static final String MOD_ID = "votresvanilla";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/votreil/votresvanilla/VotresVanillaClient.java
@@ -0,0 +1,10 @@
+package net.votreil.votresvanilla;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VotresVanillaClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
