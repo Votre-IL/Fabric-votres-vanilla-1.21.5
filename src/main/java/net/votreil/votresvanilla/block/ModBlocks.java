efd7b2cc5ae32daea01ec622f4d6909af09ea092
Votre-IL <adamgaltier@icloud.com>
2025-08-14 01:53:39 +0200

    Updated Fine Glazed Terracottas

--- src/main/java/net/votreil/votresvanilla/block/ModBlocks.java
+++ src/main/java/net/votreil/votresvanilla/block/ModBlocks.java
@@ -32,114 +32,114 @@ public class ModBlocks {
                     .mapColor(MapColor.DIRT_BROWN)
                     .strength(0.5f)
                     .sounds(BlockSoundGroup.GRAVEL)));
-    public static final Block FINE_BLACK_GLAZED_TERRACOTTA = registerBlock("fine_black_glazed_terracotta",
+    public static final Block FINE_BLACK_GLAZED_TERRACOTTA_0 = registerBlock("fine_black_glazed_terracotta_0",
             new Block(AbstractBlock.Settings.create()
-                    .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(VotresVanilla.MOD_ID, "fine_black_glazed_terracotta")))
+                    .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(VotresVanilla.MOD_ID, "fine_black_glazed_terracotta_0")))
                     .mapColor(MapColor.TERRACOTTA_BLACK)
                     .instrument(NoteBlockInstrument.BASEDRUM)
                     .strength(1.25f, 4.2f)
                     .sounds(BlockSoundGroup.STONE)));
-    public static final Block FINE_BLUE_GLAZED_TERRACOTTA = registerBlock("fine_blue_glazed_terracotta",
+    public static final Block FINE_BLUE_GLAZED_TERRACOTTA_0 = registerBlock("fine_blue_glazed_terracotta_0",
             new Block(AbstractBlock.Settings.create()
-                    .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(VotresVanilla.MOD_ID, "fine_blue_glazed_terracotta")))
+                    .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(VotresVanilla.MOD_ID, "fine_blue_glazed_terracotta_0")))
                     .mapColor(MapColor.TERRACOTTA_BLUE)
                     .instrument(NoteBlockInstrument.BASEDRUM)
                     .strength(1.25f, 4.2f)
                     .sounds(BlockSoundGroup.STONE)));
-    public static final Block FINE_BROWN_GLAZED_TERRACOTTA = registerBlock("fine_brown_glazed_terracotta",
+    public static final Block FINE_BROWN_GLAZED_TERRACOTTA_0 = registerBlock("fine_brown_glazed_terracotta_0",
             new Block(AbstractBlock.Settings.create()
-                    .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(VotresVanilla.MOD_ID, "fine_brown_glazed_terracotta")))
+                    .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(VotresVanilla.MOD_ID, "fine_brown_glazed_terracotta_0")))
                     .mapColor(MapColor.TERRACOTTA_BROWN)
                     .instrument(NoteBlockInstrument.BASEDRUM)
                     .strength(1.25f, 4.2f)
                     .sounds(BlockSoundGroup.STONE)));
-    public static final Block FINE_CYAN_GLAZED_TERRACOTTA = registerBlock("fine_cyan_glazed_terracotta",
+    public static final Block FINE_CYAN_GLAZED_TERRACOTTA_0 = registerBlock("fine_cyan_glazed_terracotta_0",
             new Block(AbstractBlock.Settings.create()
-                    .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(VotresVanilla.MOD_ID, "fine_cyan_glazed_terracotta")))
+                    .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(VotresVanilla.MOD_ID, "fine_cyan_glazed_terracotta_0")))
                     .mapColor(MapColor.TERRACOTTA_CYAN)
                     .instrument(NoteBlockInstrument.BASEDRUM)
                     .strength(1.25f, 4.2f)
                     .sounds(BlockSoundGroup.STONE)));
-    public static final Block FINE_GRAY_GLAZED_TERRACOTTA = registerBlock("fine_gray_glazed_terracotta",
+    public static final Block FINE_GRAY_GLAZED_TERRACOTTA_0 = registerBlock("fine_gray_glazed_terracotta_0",
             new Block(AbstractBlock.Settings.create()
-                    .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(VotresVanilla.MOD_ID, "fine_gray_glazed_terracotta")))
+                    .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(VotresVanilla.MOD_ID, "fine_gray_glazed_terracotta_0")))
                     .mapColor(MapColor.TERRACOTTA_GRAY)
                     .instrument(NoteBlockInstrument.BASEDRUM)
                     .strength(1.25f, 4.2f)
                     .sounds(BlockSoundGroup.STONE)));
-    public static final Block FINE_GREEN_GLAZED_TERRACOTTA = registerBlock("fine_green_glazed_terracotta",
+    public static final Block FINE_GREEN_GLAZED_TERRACOTTA_0 = registerBlock("fine_green_glazed_terracotta_0",
             new Block(AbstractBlock.Settings.create()
-                    .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(VotresVanilla.MOD_ID, "fine_green_glazed_terracotta")))
+                    .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(VotresVanilla.MOD_ID, "fine_green_glazed_terracotta_0")))
                     .mapColor(MapColor.TERRACOTTA_GREEN)
                     .instrument(NoteBlockInstrument.BASEDRUM)
                     .strength(1.25f, 4.2f)
                     .sounds(BlockSoundGroup.STONE)));
-    public static final Block FINE_LIGHT_BLUE_GLAZED_TERRACOTTA = registerBlock("fine_light_blue_glazed_terracotta",
+    public static final Block FINE_LIGHT_BLUE_GLAZED_TERRACOTTA_0 = registerBlock("fine_light_blue_glazed_terracotta_0",
             new Block(AbstractBlock.Settings.create()
-                    .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(VotresVanilla.MOD_ID, "fine_light_blue_glazed_terracotta")))
+                    .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(VotresVanilla.MOD_ID, "fine_light_blue_glazed_terracotta_0")))
                     .mapColor(MapColor.TERRACOTTA_LIGHT_BLUE)
                     .instrument(NoteBlockInstrument.BASEDRUM)
                     .strength(1.25f, 4.2f)
                     .sounds(BlockSoundGroup.STONE)));
-    public static final Block FINE_LIGHT_GRAY_GLAZED_TERRACOTTA = registerBlock("fine_light_gray_glazed_terracotta",
+    public static final Block FINE_LIGHT_GRAY_GLAZED_TERRACOTTA_0 = registerBlock("fine_light_gray_glazed_terracotta_0",
             new Block(AbstractBlock.Settings.create()
-                    .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(VotresVanilla.MOD_ID, "fine_light_gray_glazed_terracotta")))
+                    .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(VotresVanilla.MOD_ID, "fine_light_gray_glazed_terracotta_0")))
                     .mapColor(MapColor.TERRACOTTA_LIGHT_GRAY)
                     .instrument(NoteBlockInstrument.BASEDRUM)
                     .strength(1.25f, 4.2f)
                     .sounds(BlockSoundGroup.STONE)));
-    public static final Block FINE_LIME_GLAZED_TERRACOTTA = registerBlock("fine_lime_glazed_terracotta",
+    public static final Block FINE_LIME_GLAZED_TERRACOTTA_0 = registerBlock("fine_lime_glazed_terracotta_0",
             new Block(AbstractBlock.Settings.create()
-                    .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(VotresVanilla.MOD_ID, "fine_lime_glazed_terracotta")))
+                    .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(VotresVanilla.MOD_ID, "fine_lime_glazed_terracotta_0")))
                     .mapColor(MapColor.TERRACOTTA_LIME)
                     .instrument(NoteBlockInstrument.BASEDRUM)
                     .strength(1.25f, 4.2f)
                     .sounds(BlockSoundGroup.STONE)));
-    public static final Block FINE_MAGENTA_GLAZED_TERRACOTTA = registerBlock("fine_magenta_glazed_terracotta",
+    public static final Block FINE_MAGENTA_GLAZED_TERRACOTTA_0 = registerBlock("fine_magenta_glazed_terracotta_0",
             new Block(AbstractBlock.Settings.create()
-                    .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(VotresVanilla.MOD_ID, "fine_magenta_glazed_terracotta")))
+                    .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(VotresVanilla.MOD_ID, "fine_magenta_glazed_terracotta_0")))
                     .mapColor(MapColor.TERRACOTTA_MAGENTA)
                     .instrument(NoteBlockInstrument.BASEDRUM)
                     .strength(1.25f, 4.2f)
                     .sounds(BlockSoundGroup.STONE)));
-    public static final Block FINE_ORANGE_GLAZED_TERRACOTTA = registerBlock("fine_orange_glazed_terracotta",
+    public static final Block FINE_ORANGE_GLAZED_TERRACOTTA_0 = registerBlock("fine_orange_glazed_terracotta_0",
             new Block(AbstractBlock.Settings.create()
-                    .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(VotresVanilla.MOD_ID, "fine_orange_glazed_terracotta")))
+                    .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(VotresVanilla.MOD_ID, "fine_orange_glazed_terracotta_0")))
                     .mapColor(MapColor.TERRACOTTA_ORANGE)
                     .instrument(NoteBlockInstrument.BASEDRUM)
                     .strength(1.25f, 4.2f)
                     .sounds(BlockSoundGroup.STONE)));
-    public static final Block FINE_PINK_GLAZED_TERRACOTTA = registerBlock("fine_pink_glazed_terracotta",
+    public static final Block FINE_PINK_GLAZED_TERRACOTTA_0 = registerBlock("fine_pink_glazed_terracotta_0",
             new Block(AbstractBlock.Settings.create()
-                    .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(VotresVanilla.MOD_ID, "fine_pink_glazed_terracotta")))
+                    .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(VotresVanilla.MOD_ID, "fine_pink_glazed_terracotta_0")))
                     .mapColor(MapColor.TERRACOTTA_PINK)
                     .instrument(NoteBlockInstrument.BASEDRUM)
                     .strength(1.25f, 4.2f)
                     .sounds(BlockSoundGroup.STONE)));
-    public static final Block FINE_PURPLE_GLAZED_TERRACOTTA = registerBlock("fine_purple_glazed_terracotta",
+    public static final Block FINE_PURPLE_GLAZED_TERRACOTTA_0 = registerBlock("fine_purple_glazed_terracotta_0",
             new Block(AbstractBlock.Settings.create()
-                    .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(VotresVanilla.MOD_ID, "fine_purple_glazed_terracotta")))
+                    .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(VotresVanilla.MOD_ID, "fine_purple_glazed_terracotta_0")))
                     .mapColor(MapColor.TERRACOTTA_PURPLE)
                     .instrument(NoteBlockInstrument.BASEDRUM)
                     .strength(1.25f, 4.2f)
                     .sounds(BlockSoundGroup.STONE)));
-    public static final Block FINE_RED_GLAZED_TERRACOTTA = registerBlock("fine_red_glazed_terracotta",
+    public static final Block FINE_RED_GLAZED_TERRACOTTA_0 = registerBlock("fine_red_glazed_terracotta_0",
             new Block(AbstractBlock.Settings.create()
-                    .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(VotresVanilla.MOD_ID, "fine_red_glazed_terracotta")))
+                    .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(VotresVanilla.MOD_ID, "fine_red_glazed_terracotta_0")))
                     .mapColor(MapColor.TERRACOTTA_RED)
                     .instrument(NoteBlockInstrument.BASEDRUM)
                     .strength(1.25f, 4.2f)
                     .sounds(BlockSoundGroup.STONE)));
-    public static final Block FINE_WHITE_GLAZED_TERRACOTTA = registerBlock("fine_white_glazed_terracotta",
+    public static final Block FINE_WHITE_GLAZED_TERRACOTTA_0 = registerBlock("fine_white_glazed_terracotta_0",
             new Block(AbstractBlock.Settings.create()
-                    .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(VotresVanilla.MOD_ID, "fine_white_glazed_terracotta")))
+                    .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(VotresVanilla.MOD_ID, "fine_white_glazed_terracotta_0")))
                     .mapColor(MapColor.TERRACOTTA_WHITE)
                     .instrument(NoteBlockInstrument.BASEDRUM)
                     .strength(1.25f, 4.2f)
                     .sounds(BlockSoundGroup.STONE)));
-    public static final Block FINE_YELLOW_GLAZED_TERRACOTTA = registerBlock("fine_yellow_glazed_terracotta",
+    public static final Block FINE_YELLOW_GLAZED_TERRACOTT_0 = registerBlock("fine_yellow_glazed_terracotta_0",
             new Block(AbstractBlock.Settings.create()
-                    .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(VotresVanilla.MOD_ID, "fine_yellow_glazed_terracotta")))
+                    .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(VotresVanilla.MOD_ID, "fine_yellow_glazed_terracotta_0")))
                     .mapColor(MapColor.TERRACOTTA_YELLOW)
                     .instrument(NoteBlockInstrument.BASEDRUM)
                     .strength(1.25f, 4.2f)
@@ -161,21 +161,22 @@ public class ModBlocks {
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
             entries.add(CUT_ANDESITE);
-            entries.add(FINE_BLACK_GLAZED_TERRACOTTA);
-            entries.add(FINE_BLUE_GLAZED_TERRACOTTA);
-            entries.add(FINE_BROWN_GLAZED_TERRACOTTA);
-            entries.add(FINE_CYAN_GLAZED_TERRACOTTA);
-            entries.add(FINE_GREEN_GLAZED_TERRACOTTA);
-            entries.add(FINE_LIGHT_BLUE_GLAZED_TERRACOTTA);
-            entries.add(FINE_LIGHT_GRAY_GLAZED_TERRACOTTA);
-            entries.add(FINE_LIME_GLAZED_TERRACOTTA);
-            entries.add(FINE_MAGENTA_GLAZED_TERRACOTTA);
-            entries.add(FINE_ORANGE_GLAZED_TERRACOTTA);
-            entries.add(FINE_PINK_GLAZED_TERRACOTTA);
-            entries.add(FINE_PURPLE_GLAZED_TERRACOTTA);
-            entries.add(FINE_RED_GLAZED_TERRACOTTA);
-            entries.add(FINE_WHITE_GLAZED_TERRACOTTA);
-            entries.add(FINE_YELLOW_GLAZED_TERRACOTTA);
+            entries.add(FINE_BLACK_GLAZED_TERRACOTTA_0);
+            entries.add(FINE_BLUE_GLAZED_TERRACOTTA_0);
+            entries.add(FINE_BROWN_GLAZED_TERRACOTTA_0);
+            entries.add(FINE_CYAN_GLAZED_TERRACOTTA_0);
+            entries.add(FINE_GRAY_GLAZED_TERRACOTTA_0);
+            entries.add(FINE_GREEN_GLAZED_TERRACOTTA_0);
+            entries.add(FINE_LIGHT_BLUE_GLAZED_TERRACOTTA_0);
+            entries.add(FINE_LIGHT_GRAY_GLAZED_TERRACOTTA_0);
+            entries.add(FINE_LIME_GLAZED_TERRACOTTA_0);
+            entries.add(FINE_MAGENTA_GLAZED_TERRACOTTA_0);
+            entries.add(FINE_ORANGE_GLAZED_TERRACOTTA_0);
+            entries.add(FINE_PINK_GLAZED_TERRACOTTA_0);
+            entries.add(FINE_PURPLE_GLAZED_TERRACOTTA_0);
+            entries.add(FINE_RED_GLAZED_TERRACOTTA_0);
+            entries.add(FINE_WHITE_GLAZED_TERRACOTTA_0);
+            entries.add(FINE_YELLOW_GLAZED_TERRACOTT_0);
         });
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(entries -> {
             entries.add(GRASSY_DIRT);
